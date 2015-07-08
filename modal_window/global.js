42c5d7434071f8b66c9b98b843c794a8c98b6309
Ciprianna <ciprianna17@gmail.com>
2015-07-08 16:20:52 -0500

    Got the modal window functioning.

---
+++ modal_window/global.js
@@ -0,0 +1,5 @@
+// Changes the visibility property of the element (toggle)
+function overlay() {
+  modal = document.getElementById("overlay");
+  modal.style.visibility = (modal.style.visibility == "visible") ? "hidden": "visible";
+}
