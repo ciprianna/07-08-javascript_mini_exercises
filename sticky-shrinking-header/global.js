20459594657d24b7ad487d6e79bdc7e5d8faa479
Ciprianna <ciprianna17@gmail.com>
2015-07-08 17:36:13 -0500

    Finished the scrolling functionality of the header.

--- sticky-shrinking-header/global.js
+++ sticky-shrinking-header/global.js
@@ -1,20 +1,10 @@
-// function shrinkHeader(){
-//   head = document.getElementById("head");
-//   head.style.fontSize = (head.style.fontSize == "30px") ? "22px" : "30px";
-// }
-
-function init() {
-    window.addEventListener('scroll', function(e){
-        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-            shrinkOn = 500,
-            header = document.querySelector("header");
-        if (distanceY > shrinkOn) {
-            classie.add(header,"smaller");
-        } else {
-            if (classie.has(header,"smaller")) {
-                classie.remove(header,"smaller");
-            }
-        }
-    });
+function shrinkHeader(){
+  head = document.getElementById("head");
+  if (window.pageYOffset > 50 ){
+    head.style.fontSize = "22px";
+  } else {
+    head.style.fontSize = "30px";
+  }
 }
-window.onload = init();
+
+window.addEventListener("scroll", shrinkHeader);
