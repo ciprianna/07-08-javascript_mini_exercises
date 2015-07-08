d6ec5f05f0e8cebcc8bb029211e18830e1bd8513
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-08 11:31:17 -0500

    Beginning to add js function to the tabs

---
+++ tabs/global.js
@@ -0,0 +1,47 @@
+var tabLinks = new Array();
+var contentDivs = new Array();
+
+// Function sets up tabs
+//
+// Create an Array to get each line from the list tabs.
+// Iterating through listTabs, if node is list item locate anchor tag
+// and get URL link
+//
+//
+function init(){
+  var listTabs = document.getElementById("tabs").childNodes;
+  for (var i = 0; i < listTabs.length; i++){
+    if (listTabs[i].nodeName === "LI"){
+      var tabLink = getFirstChildWithTagName(listTabs[i], "A");
+      var id = getLink(tabLink.getAttribute("href"));
+      tabLinks[id] = tabLink;
+      contentDivs[id] = document.getElementById(id);
+    }
+  }
+  
+  // Starting with an index of 0, loop through tabLinks, adding
+  // showTab function to each tab.
+  var i = 0
+  
+  for (var id in tabLinks){
+    tabLinks[id].onclick = showTab;
+    if (i === 0) tabLinks[id].className = "selected";
+    i++
+  }
+}
+
+
+
+
+
+var aboutTab = document.getElementById("about")
+
+aboutTab.addEventListener("click", showTab)
+
+var contactTab = document.getElementById("contact_us")
+
+contactTab.addEventListener("click", showTab)
+
+var locationTab = document.getElementById("location")
+
+locationTab.addEventListener("click", showTab)
