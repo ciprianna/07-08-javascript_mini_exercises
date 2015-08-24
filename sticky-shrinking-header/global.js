576a3e69bbac39ab2063461b2b14e70fcf9c7e13
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-08-24 13:36:40 -0500

    Fix shrinking header

--- sticky-shrinking-header/global.js
+++ sticky-shrinking-header/global.js
@@ -1,6 +1,6 @@
 function shrinkHeader(){
   head = document.getElementById("head");
-  if (window.pageYOffset > 50 ){
+  if (window.pageYOffset > 40 ){
     head.style.fontSize = "22px";
   } else {
     head.style.fontSize = "30px";
